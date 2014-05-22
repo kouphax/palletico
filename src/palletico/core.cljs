c3182c0074ff5d0ac57f34190b72a0fb3b90f25c
James Hughes <james@yobriefca.se>
2014-05-22 21:21:42 +0100

    Code Rework and Styling Tweaks
    So this is the first kinda-ready version of pallettico.  I need to sort
    out the fact I have an inconsistent approach to the word pallette
    because i've spelled it a million different ways.
    
    I wish I could design nice things.

--- src/palletico/core.cljs
+++ src/palletico/core.cljs
@@ -6,80 +6,49 @@
 
 (defn button [state]
   (let [visible? (:list-visible @state)]
-    [:a.menu-button
+    [:button.menu-button
       { :on-click #(swap! state assoc :list-visible (not visible?)) }
       (if visible? "↑" "↓")]))
 
+(defn mini-pallette [colours]
+  [:div.mini-palette
+    (for [colour colours]
+      [:div { :key colour
+              :style { :background-color  colour } }])])
+
 (defn menu [state]
-  (let [handler (fn [value] #(swap! state assoc :selected-value value :list-visible false)) ]
-  [:ul { :style { :display (if (:list-visible @state) "inherit" "none") } }
-    [:li { :on-click (handler "1") } "one"]
-    [:li { :on-click (handler "2") } "two"]
-    [:li { :on-click (handler "3") } "three"]
-    [:li { :on-click (handler "4") } "four"]]))
+  (let [handler (fn [value] #(swap! state assoc :selected value :list-visible false)) ]
+    [:ul { :style { :display (if (:list-visible @state) "inherit" "none") } }
+      (for [[name pallette] (:pallettes @state)]
+        [:li { :on-click (handler pallette) }
+          [:div.pallette-name name]
+          [mini-pallette pallette]])]))
 
-(defn selected-value [state]
-  [:div (:selected-value @state)])
+(defn selected [state]
+  [:div.mega-palette
+   (let [colours (:selected @state)]
+    (for [colour  colours
+            :let [width     (/ 100 (count colours))
+                  width-pct (str width "%")]]
+        [:div { :key colour
+                :style { :background-color  colour
+                          :width            width-pct
+                          :display          :inline-block
+                          :height           "100%" } } ] ))])
 
-(defn container []
+(defn container [pallettes]
   (let [state (atom { :list-visible false
-                      :selected-value "" })]
+                      :selected     nil
+                      :pallettes    pallettes })]
     (fn []
       [:div
-       [button state]
-       [menu state]
-       [selected-value state]])))
+       [:div.menu
+         [button state]
+         [menu state]]
+       [selected state]])))
 
-(GET "/palletes.json" { :handler (reagent/render-component
-                                   [container]
-                                   (.-body js/document))
+(GET "/palletes.json" { :handler #(reagent/render-component
+                                    [container %]
+                                    (.-body js/document))
                         :response-format :json
                         :error-handler println })
-
-
-
-
-
-;(def selected-palette
-;  (atom nil))
-;
-;(defn mini-palette [colours]
-;  [:div.mini-palette
-;    (for [colour colours]
-;      [:div { :key colour
-;              :style { :background-color  colour } }])])
-;
-;(defn mega-palette [colours]
-;  [:div.mega-palette
-;    (for [colour colours
-;          :let [width     (/ 100 (count colours))
-;                width-pct (str width "%")]]
-;      [:div { :key colour
-;              :style { :background-color  colour
-;                       :width             width-pct
-;                       :display           :inline-block
-;                       :height            "100%" } }])])
-;
-;; use sidr
-;(defn palette-list [palettes]
-;  [:ul
-;    (for [[name colours] palettes]
-;      [:li { :key name }
-;        [:a.palette-name { :on-click #(reset! selected-palette colours)
-;                           :href "#" }  name]
-;        [mini-palette colours]])])
-;
-;(defn application [pallettes]
-;  [:div#application
-;    [palette-list pallettes]
-;    [mega-palette @selected-palette]])
-;
-;(defn render-application [pallettes]
-;  (reagent/render-component
-;    [application pallettes]
-;    (.-body js/document)))
-;
-;(GET "/palletes.json" { :handler render-application
-;                        :response-format :json
-;                        :error-handler println })
-;
